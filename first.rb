322083f07cb35efd202f927a5a25c4c46954f20d
Eduardo-Castillo-Sfleet <eduardo.castillo@sfleet.mx>
2021-01-27 01:22:56 -0600

    array

--- first.rb
+++ first.rb
@@ -6,13 +6,31 @@ programador = true
 conoceRuby = nil #null value
 
 
-puts (nombre[-2].upcase() + nombre + " tiene " + edad.to_s)
-puts edad - 5
+#puts (nombre[-2].upcase() + nombre + " tiene " + edad.to_s)
+#puts edad - 5
+#
+#puts "Ingresa tu nombre: "
+#nombre = gets.chomp() #quits enter on input
+#puts "Nuevo nombre: {nombre} eres genial"
+#
+#puts "Ingresa tu edad: "
+#edad = gets.chomp() #quits enter on input
+#puts "Nuevo edad:" + edad + " eres viejol"
+#
+#puts "Ingresa num1: "
+#num1 = gets.chomp().to_i #quits enter on input
+#puts "Ingresa num2: "
+#num2 = gets.chomp().to_i #quits enter on input
+#puts num1+num2
 
-puts "Ingresa tu nombre: "
-nombre = gets.chomp() #quits enter on input
-puts "Nuevo nombre: " + nombre + " eres genial"
 
-puts "Ingresa tu edad: "
-edad = gets.chomp() #quits enter on input
-puts "Nuevo edad: " + edad.to_s + " eres genial"
+#puts "Ingresa posicion: "
+#posicion = gets.chomp().to_i #quits enter on input
+#elArray = Array["armando", 18, false]
+#puts elArray[posicion]
+#puts elArray[0,2]
+#
+#puts elArray.reverse()
+
+numerosP = Array[5,3,7,2,11]
+puts numerosP.sort()
