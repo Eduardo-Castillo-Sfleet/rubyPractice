bb6064e34edf98d67670699db81b7ba75cf61a64
Eduardo-Castillo-Sfleet <eduardo.castillo@sfleet.mx>
2021-01-27 20:22:57 -0600

    moduls and irb

---
+++ school.rb
@@ -0,0 +1,5 @@
+require_relative "user.rb"
+
+include User
+
+User.greeting("Armando")
---
+++ user.rb
@@ -0,0 +1,11 @@
+module User
+
+    def greeting(name)
+        puts "hi #{name}"
+    end
+
+    def bye(name)
+        puts "bye #{name}"
+    end
+
+end
